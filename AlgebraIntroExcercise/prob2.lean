4fb70c188721bd5ee153c593e7faac60e8417fdb
Haruki Oshiro <harukioshiro875421@gmail.com>
2025-10-12 20:53:50 +0900

    しょぼい証明たち

--- AlgebraIntroExcercise/prob2.lean
+++ AlgebraIntroExcercise/prob2.lean
@@ -13,6 +13,7 @@
 (1) Zのイデアルの定義を述べよ
 (2) a₁, a₂, ..., a_k ∈ ℤ とする I = {n₁a₁ + ... + n_k a_k | n₁, n₂, ..., n_k ∈ ℤ }はZのイデアルであることを示せ
 -/
+import Mathlib.Data.Int.GCD -- Nat.xgcd
 
 -- 定理: gcd(391, 667) = 23である
 theorem prob_2_1_1: Nat.gcd 391 667 = 23 := by
@@ -23,3 +24,14 @@ theorem prob_2_1_1: Nat.gcd 391 667 = 23 := by
 -- 定理: gcd(2431, 4199) = 221
 theorem prob_2_1_2: Nat.gcd 2431 4199 = 221 := by
   rfl
+
+
+#eval Nat.gcd 391 667 -- 23
+#eval Nat.xgcd 391 667 -- 12, -7
+#eval 391 * 12 + 667 * -7 -- 23
+
+theorem prob_2: 391 * 12 + 667 * (-7 : ℤ ) = Int.gcd 391 667 := by
+  -- goal: 391 * 7 + 667 * -4 = ↑(Int.gcd 391 667)
+  have h1 : Int.gcd 391 667 = 23 := by
+    rfl
+  simp [h1] -- 証明完了
